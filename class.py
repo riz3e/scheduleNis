e88d1b860961d9adae9e38ad0e662e70b2e96265
qrob <abdraximm@gmail.com>
2023-10-15 13:57:32 +0600

    0.6(finally works)

--- class.py
+++ class.py
@@ -1,7 +1,9 @@
 import json
-
 import requests
 from datetime import datetime, timedelta
+
+link = "https://nistaldykorgan.edupage.org/timetable/server/currenttt.js?__func=curentttGetData"
+classid = "-140"
 
 # Get the current date
 current_date = datetime.now()
@@ -16,10 +18,7 @@ end_of_week = start_of_week + timedelta(days=6)
 start_of_week_str = start_of_week.strftime("%Y-%m-%d")
 end_of_week_str = end_of_week.strftime("%Y-%m-%d")
 
-link = "https://nistaldykorgan.edupage.org/timetable/server/currenttt.js?__func=curentttGetData"
-
 session = requests.Session()
-
 headers = {
     "Content-Type": "text/plain",
     "Content-Length": "0",
@@ -30,7 +29,6 @@ headers = {
     "Cookie": "PHPSESSID=e645fe0824df28dff6798f7df59c406f",
     "Host": "nistaldykorgan.edupage.org"
 }
-
 payload = {
     "__args": [
         None,
@@ -39,7 +37,7 @@ payload = {
             "datefrom": start_of_week_str,
             "dateto": end_of_week_str,
             "table": "classes",  # needed table name
-            "id": "-140",  # needed class id(fetched from db)
+            "id": classid,  # needed class id(fetched from db)
             "showColors": True,  # Responcible for colors
             "showIgroupsInClasses": False,  # idk what it does
             "showOrig": True,  # is responsible for the “Canceled” parameter, I don’t know what it means
@@ -50,12 +48,10 @@ payload = {
     "__gsh": "00000000"
 }
 
-response = requests.post(url=link, params=json.dumps(payload), headers=headers)
+response = requests.post(url=link, data=json.dumps(payload), headers=headers)
 # print(json.dumps(payload, indent=4))
 if response.status_code == 200:
-    data = response.text
-    print(data)
-    # print(payload)
+    data = response.json()
+    print(json.dumps(data, indent=4))
 else:
     print(f"Failed to retrieve data. Status code: {response.status_code}")
-
